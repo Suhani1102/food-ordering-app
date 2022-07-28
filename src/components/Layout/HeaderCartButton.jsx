07324c3784999a01efe06ab85cf6166b922a52f3
Suhani1102 <sandhusuhani1102@gmail.com>
2022-07-28 23:44:30 +0530

    cart btn animation

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,15 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../contexts/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnAnimation, setBtnAnimation] = useState(false);
   const cartCtx = useContext(CartContext);
-  const noOfCartItems = cartCtx.items.reduce((currNum, item) => {
+  const { items } = cartCtx;
+  const noOfCartItems = items.reduce((currNum, item) => {
     return currNum + item.amount;
   }, 0);
+
+  const btnClasses = `${classes.button} ${btnAnimation ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnAnimation(true);
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
